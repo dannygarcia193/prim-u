e427ef11d756262c495d1defa2b0c66cec79e24d
Daniel Garcia Rodriguez <dannygarcia193@gmail.com>
2021-06-16 14:21:06 -0700

    Draft 2 of card container. Adhered to guideline. Needs additional responsive breakpoint css rules.

--- src/components/customers/Customer.js
+++ src/components/customers/Customer.js
@@ -6,11 +6,13 @@ const Customer = () => {
         <div className={styles.Card}>
             <div className={styles.TextContainer}>
                 <p className={styles.Heading}>FOR CUSTOMERS</p>
-                <p className={styles.Text}>Enjoy beauty treatments wherever U are</p>
-                <p className={styles.Text}>Struggling to find the time to pamper yourself? 
-                With just one click, Prim-U will connect U to the right primlancer, at the right time, right in the comfort of your home. 
-                On holiday? Book an expert primlancer closest to U. 
-                Simply click, pick a treatment and we’ll come to U.</p>
+                <div className={styles.DescriptionContainer}>
+                    <p className={styles.Text}>Enjoy beauty treatments wherever U are</p>
+                    <p className={styles.Text}>Struggling to find the time to pamper yourself? 
+                    With just one click, Prim-U will connect U to the right primlancer, at the right time, right in the comfort of your home. 
+                    On holiday? Book an expert primlancer closest to U. 
+                    Simply click, pick a treatment and we’ll come to U.</p>
+                </div>
             </div>
             <div className={styles.ButtonContainer}>
                 <button className={styles.CTAButton}><span className={styles.ButtonText}>Make a Booking</span></button>
