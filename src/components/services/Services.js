67a341a270416908f9d0b72539e4679b9cf55285
Daniel Garcia Rodriguez <dannygarcia193@gmail.com>
2021-06-17 09:49:03 -0700

    Added mockup for gallery

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,15 +1,21 @@
 import * as styles from "./Services.module.css"
 import React from "react"
+
+const ServicesText = ["massage", "WAXING", "make up", "manicure", "hair", "male grooming"]
+const ServicesImage = ["image2.svg","image3.svg", "Rectangle8.svg", "image4.svg","image5.svg","image6.svg"]
 const Gallery = () =>{
+    const image = ServicesImage.map( (imageURL, idx)=> {
+        return (
+            <div className={styles.ImageContainer}>
+                <div className={styles.Overlay}>
+                <p className={styles.Service}>{ServicesText[idx]}</p>
+                </div>
+            </div>
+        )
+    })
     return(
         <div className={styles.Gallery}>
-        <div className={styles.ImageContainer}/>
-        <div className={styles.ImageContainer}/>
-        <div className={styles.ImageContainer}/>
-        <div className={styles.ImageContainer}/>
-        <div className={styles.ImageContainer}/>
-        <div className={styles.ImageContainer}/>
-        <div className={styles.ImageContainer}/>
+            {image}
         </div>
     )
 }
