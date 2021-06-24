88b34a89422fb882012d5a3a6b7a2e0acc29f305
Daniel Garcia Rodriguez <dannygarcia193@gmail.com>
2021-06-24 14:12:48 -0700

    Fixed z index

--- src/components/card/ForCustomers.js
+++ src/components/card/ForCustomers.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import Card from "./Card";
-import { HR } from "./Card.module.css";
 const ForCustomers = () => {
   const data = useStaticQuery(graphql`
     {
